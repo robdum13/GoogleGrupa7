50af71202e7dea00a9a70e6a72768cd735addd83
Robert <robert.dumitru2002@gmail.com>
2023-04-06 19:01:03 +0300

    test

---
+++ week1/operatori.py
@@ -0,0 +1,13 @@
+print ("Hello")
+#print ("Hello")
+print (3.0,type(3.0))
+print (3 + 1j, type(3 + 1j))
+print(1-2)
+print (1%2)
+print (1//2) # catul
+print (1**2) #putere
+print (1==2) #adevarat sau fals
+print(1!=2)
+
+
+

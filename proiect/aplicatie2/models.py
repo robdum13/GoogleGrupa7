c6af5fb3ece429aa23ca011bdc8007c58928961a
Robert <robert.dumitru2002@gmail.com>
2023-05-11 22:28:47 +0300

    '.'

--- proiect/aplicatie2/models.py
+++ proiect/aplicatie2/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from aplicatie1.models import Location
+
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- proiect/aplicatie2/views.py
+++ proiect/aplicatie2/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import CompanyForm
 # Create your views here.
+
+def add_company(request):
+    if request.method == 'POST':
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('company_list')
+    else:
+        form = CompanyForm()
+    return render(request, 'add_company.html', {'form': form})
